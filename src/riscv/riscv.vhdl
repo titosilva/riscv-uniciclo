2be792680aa97f58c77c4825095067b3cb086217
titosilva <jt.mat@hotmail.com>
2021-05-22 11:38:08 -0300

    feat: added top level riscv entity

---
+++ src/riscv/riscv.vhdl
@@ -0,0 +1,17 @@
+library IEEE;
+use IEEE.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+library work;
+use work.riscv_components.all;
+
+entity RISCV is
+  port (
+    clock: in std_logic
+  );
+end RISCV;
+
+architecture RTL of ent is
+begin
+  --process
+end RTL;
