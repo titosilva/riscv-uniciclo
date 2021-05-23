06856b4ee3b3e307747dbe6f490e9e0779c407db
titosilva <jt.mat@hotmail.com>
2021-05-23 08:15:42 -0300

    feat: added I type and B type to Control

--- src/components/Control/Control.vhdl
+++ src/components/Control/Control.vhdl
@@ -8,7 +8,7 @@ entity Control is
     branch: out std_logic;
     memRead: out std_logic;
     memReg: out std_logic;
-    ULAOp: out std_logic;
+    ULAOp: out std_logic_vector(1 downto 0);
     ULASrc: out std_logic;
     memWrite: out std_logic;
     regWrite: out std_logic
@@ -20,13 +20,33 @@ begin
   root_proc: process(opcode) is
   begin
     case opcode is
-      -- ADD
+      -- R-type
       when "0110011" =>
-        branch <= '0';
-        memRead <= '0';
+        ULASrc <= '0';
         memReg <= '0';
-        memWrite <= '0';
         regWrite <= '1';
+        memRead <= '0';
+        memWrite <= '0';
+        branch <= '0';
+        ULAOp <= "10";
+      -- I-type
+      when "0000011" =>
+        ULASrc <= '1';
+        memReg <= '0';
+        regWrite <= '1';
+        memRead <= '0';
+        memWrite <= '0';
+        branch <= '0';
+        ULAOp <= "10";
+      -- B-type
+      when "1100011" =>
+        ULASrc <= '0';
+        memReg <= '0';
+        regWrite <= '0';
+        memRead <= '0';
+        memWrite <= '0';
+        branch <= '1';
+        ULAOp <= "01";
       when others =>
         branch <= '0';  
         memRead <= '0';
